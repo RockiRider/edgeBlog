6bdce347e269fc42723a1370bd67cc47eccfccd7
Tsotne <tsotne24@hotmail.com>
2019-11-18 05:00:36 +0000

    okta added

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const { config, engine } = require('express-edge');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const Post = require('./database/models/Post');
 const fileUpload = require("express-fileupload");
+const session = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
 
 const app = express();
+const port = 3000;
+// session support is required to use ExpressOIDC
+
+app.use(session({
+    secret: process.env.RANDOM_SECRET_WORD,
+    resave: true,
+    saveUninitialized: false
+}));
+
+const oidc = new ExpressOIDC({
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: process.env.REDIRECT_URL,
+    scope: 'openid profile',
+    routes: {
+        callback: {
+            path: '/authorization-code/callback',
+            defaultRedirect: '/posts/new'
+        }
+    }
+});
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
+app.use(oidc.router);
+app.use(cors());
 app.use(fileUpload());
 app.use(express.static(__dirname + '/public/'));
 app.use(engine);
@@ -29,6 +57,9 @@ app.use('/posts/store', storePost)
 
 /* 
 
+
+
+
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", createPostController);
@@ -43,7 +74,8 @@ app.get('/', async (req, res) => {
     })
 });
 
-app.get('/posts/new', (req, res) => {
+
+app.get('/posts/new',oidc.ensureAuthenticated(), (req, res) => {
     res.render('create')
 });
 
@@ -54,7 +86,7 @@ app.get('/post/:id', async (req, res) => {
     })
 });
 
-app.post("/posts/store", (req, res) => {
+app.post("/posts/store",oidc.ensureAuthenticated(), (req, res) => {
     const {
         image
     } = req.files
@@ -69,6 +101,4 @@ app.post("/posts/store", (req, res) => {
     })
 });
 
-app.listen(4000, () => {
-    console.log('App listening on port 4000')
-});
+app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
